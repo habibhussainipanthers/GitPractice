af2eb15cc2e1d15202cd47db015c4234a8c3e3ec
hussaini <habib.hussaini@tekschool.us>
2022-11-10 12:33:34 -0800

    this is the 4th changes

--- src/gitPractis/FirstClassGit.java
+++ src/gitPractis/FirstClassGit.java
@@ -4,6 +4,11 @@ public class FirstClassGit {
 
 	public static void main(String[] args) {
 		System.out.println("this is a test.");
+		int a = 12;
+		int b = 30;
+		int c = a + b;
+		System.out.println(c);
+		
 
 	}
 
